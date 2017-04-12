0eb4c43ee311769aac6de3dda3c74fbe071c493f
samuel grahame <samuel@intelogy.com.au>
2017-04-12 13:58:50 +1000

    Added Table Header data

---
+++ ExpressCraft.Bootstrap/Table/TableHeaderData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge;
+using Bridge.Html5;
+
+namespace ExpressCraft.Bootstrap
+{
+	public class TableHeaderData : BootstrapDiv
+	{
+		public TableHeaderData(params Union<string, Control, HTMLElement>[] typos) : base(new HTMLTableHeaderCellElement(), typos)
+		{
+
+		}
+
+		public TableHeaderData(BootstrapRowCellTheme theme, params Union<string, Control, HTMLElement>[] typos) : base(new HTMLTableHeaderCellElement() { ClassName = theme.ToString("G") }, typos)
+		{
+
+		}
+	}
+}
